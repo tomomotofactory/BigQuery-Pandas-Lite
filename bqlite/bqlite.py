874a84a5e64b976d652b063bdd9956fe7c6fd03b
01016207 <tomomoto@r.recruit.co.jp>
2017-05-19 17:00:27 +0900

    refactoring bug fix in 0 rows case

--- bqlite/bqlite.py
+++ bqlite/bqlite.py
@@ -1,13 +1,13 @@
 import google.auth
 from google.cloud import bigquery
+from google.cloud.bigquery.schema import SchemaField
+from .bqlite_table import BQLiteTable
+from google.oauth2 import service_account
 import pandas as pd
 import numpy as np
 import time
-import datetime
 import logging
-from google.cloud.bigquery.schema import SchemaField
-from google.oauth2 import service_account
-from .bqlite_table import BQLiteTable
+import datetime
 
 
 class BQLite:
@@ -36,15 +36,15 @@ class BQLite:
         :param dataset_name: BigQuery`s dataset name
         :param table_name: BigQuery`s table name
         """
-        client = self._prepare_client(project_name)
-        dataset = BQLite._prepare_dataset(client, dataset_name)
+        client = self.__prepare_client(project_name)
+        dataset = BQLite.__prepare_dataset(client, dataset_name)
 
         table = dataset.table(table_name)
         if table.exists(client=client):
             raise Exception('Table {}:{} already exist.'.format(dataset_name, table_name))
 
         table = dataset.table(table_name)
-        schema = BQLite._to_bq_schema(schema_df)
+        schema = BQLite.__to_bq_schema(schema_df)
         table.schema = schema
 
         table.create(client=client)
@@ -56,8 +56,8 @@ class BQLite:
         :param dataset_name: BigQuery`s dataset name
         :param table_name: BigQuery`s table name
         """
-        client = self._prepare_client(project_name)
-        dataset = BQLite._prepare_dataset(client, dataset_name)
+        client = self.__prepare_client(project_name)
+        dataset = BQLite.__prepare_dataset(client, dataset_name)
 
         table = dataset.table(table_name)
         if not table.exists(client=client):
@@ -75,7 +75,7 @@ class BQLite:
         :param max_results: query parameter
         :return: dataFrame of query result
         """
-        client = self._prepare_client(project_name)
+        client = self.__prepare_client(project_name)
 
         # run sql
         query = client.run_sync_query(sql)
@@ -85,7 +85,7 @@ class BQLite:
         query.run(client=client)
 
         if not query.complete:
-            BQLite._wait_for_job(query.job)
+            BQLite.__wait_for_job(query.job)
 
         # get result
         rows, total_rows, page_token = query.fetch_data()
@@ -96,10 +96,35 @@ class BQLite:
             for row_index in range(len(rows)):
                 rows[row_index].extend(tmp_rows[row_index])
 
-        if query.schema is None:
-            return None
+        return BQLite.__to_df(rows, query.schema)
 
-        return BQLite._to_df(rows, query.schema)
+    def to_bq(self, load_df, project_name, dataset_name, table_name):
+        """
+        write data to BigQuery. If BigQuery`s table doesn`t exist method create BigQuery`s table.
+        :param load_df:
+        :param project_name:
+        :param dataset_name:
+        :param table_name:
+        """
+        client = self.__prepare_client(project_name)
+        dataset = BQLite.__prepare_dataset(client, dataset_name)
+
+        table = dataset.table(table_name)
+        if table.exists(client):
+            table.reload(client)
+        else:
+            self.__logger.info('Table {}:{} does not exist.'.format(dataset_name, table_name))
+            table.schema = BQLite.__to_bq_schema(load_df)
+            table.create()
+            self.__logger.info('Create Table {}:{}.'.format(dataset_name, table_name))
+
+        # cast to original table class for using null value.
+        table.__class__ = BQLiteTable
+        rows = load_df.values
+        errors = table.insert_data(rows, client=client)
+        if errors:
+            raise Exception(errors)
+        self.__logger.info('Loaded data into {}:{}'.format(dataset_name, table_name))
 
     def read_to_bq(self, sql, project_name, write_dataset_name, write_table_name, write_disposition='WRITE_TRUNCATE',
                    use_legacy_sql=False, use_query_cache=False, max_results=None, wait_timeout=1800):
@@ -115,12 +140,12 @@ class BQLite:
         :param max_results: query option about select
         :param wait_timeout: waiting time for finishing job (sec)
         """
-        client = self._prepare_client(project_name)
+        client = self.__prepare_client(project_name)
 
         write_dataset = client.dataset(write_dataset_name)
         write_table = write_dataset.table(write_table_name)
-        job_name = write_dataset_name + '_' + write_table_name + "_read_to_bq"\
-                   + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        job_name = write_dataset_name + '_' + write_table_name + "_read_to_bq" +\
+                   datetime.datetime.now().strftime('%Y%m%d%H%M%S')
         # run sql
         query = client.run_async_query(job_name, sql)
         query.use_legacy_sql = use_legacy_sql
@@ -136,99 +161,80 @@ class BQLite:
             time.sleep(10)
             query.reload()
 
-    def to_bq(self, load_df, project_name, dataset_name, table_name):
-        """
-        write data to BigQuery. If BigQuery`s table doesn`t exist method create BigQuery`s table.
-        :param load_df: loading pandas.Dataframe
-        :param project_name: write project name
-        :param dataset_name: write dataset name
-        :param table_name: write table name
-        """
-        client = self._prepare_client(project_name)
-        dataset = BQLite._prepare_dataset(client, dataset_name)
+        if query.state != 'DONE':
+            self.__logger.warn('Timeout. Write data into {}:{}'.format(write_dataset_name, write_table_name))
 
-        table = dataset.table(table_name)
-        if table.exists(client):
-            table.reload(client)
-        else:
-            self.__logger.info('Table {}:{} does not exist.'.format(dataset_name, table_name))
-            table.schema = BQLite._to_bq_schema(load_df)
-            table.create()
-            self.__logger.info('Create Table {}:{}.'.format(dataset_name, table_name))
+    def __prepare_client(self, project_name: str):
+        return bigquery.Client(project=project_name, credentials=self.__generate_credential())
 
-        # cast to original table class for using null value.
-        table.__class__ = BQLiteTable
-        rows = load_df.values
-        errors = table.insert_data(rows, client=client)
-        if errors:
-            raise Exception(errors)
-        self.__logger.info('Loaded data into {}:{}'.format(dataset_name, table_name))
-
-    def _prepare_client(self, project_name: str):
-        return bigquery.Client(project=project_name, credentials=self._generate_credential())
-
-    def _generate_credential(self):
+    def __generate_credential(self):
         if self.__credentials is not None:
-            return self.__credentials
+            pass
 
         elif self.__json_key_filepath is not None:
-            return service_account.Credentials.from_service_account_file(self.__json_key_filepath)
+            self.__credentials = service_account.Credentials.from_service_account_file(self.__json_key_filepath)
 
-        # no setup parameter case use default credentials
-        self.__logger.info("no setup parameter case get credentials by google_auth.default()")
-        credentials, df_project = google.auth.default()
-        return credentials
+        else:
+            # no setup parameter case use default credentials
+            self.__logger.info('no setup parameter case get credentials by google_auth.default()')
+            self.__credentials, df_project = google.auth.default()
+
+        return self.__credentials
 
     @staticmethod
-    def _prepare_dataset(client, dataset_name: str):
+    def __prepare_dataset(client, dataset_name: str):
         dataset = client.dataset(dataset_name)
         if not dataset.exists():
             raise Exception('Dataset {} does not exist.'.format(dataset_name))
         return dataset
 
     @staticmethod
-    def _to_df(rows, schema) -> pd.DataFrame:
+    def __to_df(rows, schema) -> pd.DataFrame:
 
-        def _to_flg(_val) -> bool:
-            return _val == 'true'
+        def to_flg(value) -> bool:
+            return value == 'true'
 
-        def _cast_all_column(values, values_type, row_no=None):
-            _cast_mapping = {
+        def cast_all_column(values, values_type, row_no=None):
+            cast_mapping = {
                 'STRING':    (str,          None),
                 'DATETIME':  (str,          None),
                 'DATE':      (str,          None),
                 'TIME':      (str,          None),
                 'FLOAT':     (float,        np.dtype(float)),
                 'INTEGER':   (float,        np.dtype(float)),
-                'BOOLEAN':   (_to_flg,      np.dtype(bool)),
+                'BOOLEAN':   (to_flg,       np.dtype(bool)),
                 'TIMESTAMP': (pd.Timestamp, 'datetime64[ns]')
             }
-            _cast_func, _col_new_type = _cast_mapping.get(values_type, (None, None))
+            cast_function, new_values_type = cast_mapping.get(values_type, (None, None))
 
-            if _cast_func is None:
+            if cast_function is None:
                 raise Exception('Not Support to type:' + values_type + '.')
-            values = _cast_column(values, _cast_func, row_no)
+            values = cast_column(values, cast_function, row_no)
 
-            return values, _col_new_type
+            return values, new_values_type
 
-        def _cast_column(values, cast_function, row_no=None):
+        def cast_column(values, cast_function, row_no=None):
             if row_no is None:
                 # is not repeated
-                for _row_no in range(len(values)):
-                    if values[_row_no] is not None:
-                        values[_row_no] = cast_function(values[_row_no])
+                for tmp_row_no in range(len(values)):
+                    if values[tmp_row_no] is not None:
+                        values[tmp_row_no] = cast_function(values[tmp_row_no])
                 return values
 
             else:
                 # is repeated
-                _col_values = [None for x in range(len(row_values))]
+                repeat_values = [None] * len(row_values)
                 if row_no is None:
                     raise Exception('Repeated column need _no_in_row parameter.')
 
-                for _row_no in range(len(values)):
-                    if row_no < len(values[_row_no]) and values[_row_no][row_no] is not None:
-                        _col_values[_row_no] = cast_function(values[_row_no][row_no])
-                return _col_values
+                for i, value in enumerate(values):
+                    if row_no < len(value) and value[row_no] is not None:
+                        repeat_values[i] = cast_function(value[row_no])
+
+                return repeat_values
+
+        if len(rows) == 0:
+            return pd.DataFrame()
 
         columns_data = []
         for index, col_dict in enumerate(schema):
@@ -241,19 +247,19 @@ class BQLite:
             if col_mode == 'REPEATED':
                 max_row_length = max([len(row_value) for row_value in row_values])
                 for no_in_row in range(max_row_length):
-                    col_values, col_new_type = _cast_all_column(row_values, col_type, row_no=no_in_row)
+                    col_values, col_new_type = cast_all_column(row_values, col_type, row_no=no_in_row)
                     columns_data.append((col_name+"_"+str(no_in_row), pd.Series(data=col_values, dtype=col_new_type)))
                     del col_values
 
             else:
-                row_values, col_new_type = _cast_all_column(row_values, col_type)
+                row_values, col_new_type = cast_all_column(row_values, col_type)
                 columns_data.append((col_name, pd.Series(data=row_values, dtype=col_new_type)))
                 del col_name, row_values
 
         return pd.DataFrame.from_items(columns_data)
 
     @staticmethod
-    def _to_bq_schema(schema_df: pd.DataFrame):
+    def __to_bq_schema(schema_df: pd.DataFrame):
         type_mapping = {
             'i': 'INTEGER',
             'b': 'BOOLEAN',
@@ -271,7 +277,7 @@ class BQLite:
         return fields
 
     @staticmethod
-    def _wait_for_job(job):
+    def __wait_for_job(job):
         while True:
             job.reload()
             if job.state == 'DONE':
